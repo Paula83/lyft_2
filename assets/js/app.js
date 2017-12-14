411a527a0a0613b2080247c5041cd7cca9e60de2
marcecabelloherrera <marcecabello.h@gmail.com>
2017-12-14 11:24:44 -0300

    segunda versión

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
- $(document).ready(function() {
-    $('select').material_select();
-  });
+	 $(document).ready(function(){
+	 	setTimeout(function() {
+	 		$('.container_logo').fadeOut(2000);
+	 	}, 2000);
+
+	 	setTimeout(function(){
+	 		$('#primero').show();
+	 	}, 4000);
+
+	 	$('#btn').click(function(){
+	 		$('#primero').hide();
+	 	});
+	 	$('select').material_select('destroy');
+
+	});
+
+
